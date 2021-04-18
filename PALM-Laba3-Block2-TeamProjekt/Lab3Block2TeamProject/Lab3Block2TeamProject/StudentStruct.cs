07dfc5c5dcd0b92272dde02969200cc1054d5402
Rostok <79173041+kopiychenkor@users.noreply.github.com>
2021-04-18 20:14:01 +0300

    struct

--- PALM-Laba3-Block2-TeamProjekt/Lab3Block2TeamProject/Lab3Block2TeamProject/StudentStruct.cs
+++ PALM-Laba3-Block2-TeamProjekt/Lab3Block2TeamProject/Lab3Block2TeamProject/StudentStruct.cs
@@ -14,6 +14,18 @@ namespace Laba3Block2
 		public char informaticsMark;
 		public int scholarship;
 
-		
+		public Student(string LineWithAllData)
+		{
+			string[] dataSplitted = LineWithAllData.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+			surName = dataSplitted[0];
+			firstName = dataSplitted[1];
+			patronymic = dataSplitted[2];
+			sex = Convert.ToChar(dataSplitted[3]);
+			dateOfBirth = dataSplitted[4];
+			mathematicsMark = Convert.ToChar(dataSplitted[5]);
+			physicsMark = Convert.ToChar(dataSplitted[6]);
+			informaticsMark = Convert.ToChar(dataSplitted[7]);
+			scholarship = Convert.ToInt32(dataSplitted[8]);
+		}
 	}
 }
